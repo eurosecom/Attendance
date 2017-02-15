14d7400646cf2bb05be13782abfa8666608109fa
eurosecom <eurosecom1@gmail.com>
2017-02-15 19:56:19 +0100

    Main Activity Attendance
    #2
    - SplashScree.java by use RxJava DisposableObserver and
    Observable.just(true).map().

--- app/src/main/java/com/eusecom/attendance/SplashScreen.java
+++ app/src/main/java/com/eusecom/attendance/SplashScreen.java
@@ -8,6 +8,11 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -16,65 +21,80 @@ public class SplashScreen extends Activity {
 
     private final int SPLASH_TIME_OUT = 1000;
     private FirebaseAuth mAuth;
+    private CompositeDisposable disposables = new CompositeDisposable();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
 
-        View backgroundImage = findViewById(R.id.background);
-        Drawable background = backgroundImage.getBackground();
-        background.setAlpha(20);
 
-        mAuth = FirebaseAuth.getInstance();
-        new Handler().postDelayed(new Runnable() {
+        DisposableObserver<Boolean> d = getDisposableObserver();
 
-            /*
-             * Showing splash screen with a timer. This will be useful when you
-             * want to show case your app logo / company
-             */
+        getObservable()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(d);
+
+        disposables.add(d);
+
+    }//end oncreate
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        disposables.clear();
+    }
+
+
+    private Observable<Boolean> getObservable() {
+        return Observable.just(true).map(aBoolean -> {
+            doLongOperation();
+            return aBoolean;
+        });
+    }
+
+    /**
+     * Observer handles the result through the 3 important actions:
+     * onCompleted, onError, onNext
+     */
+    private DisposableObserver<Boolean> getDisposableObserver() {
+        return new DisposableObserver<Boolean>() {
 
             @Override
-            public void run() {
-                // This method will be executed once the timer is over
-                // Start your app main activity
-                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+            public void onComplete() {
+
+                Log.d("onComplete", " in");
+                Log.d("SplashScreen", " Start MainActivity");
+                Intent i = new Intent(SplashScreen.this,MainActivity.class);
                 startActivity(i);
-
-                // close this activity
                 finish();
+
             }
-        }, SPLASH_TIME_OUT);
-        //new PrefetchData().execute();
 
+            @Override
+            public void onError(Throwable e) {
+                Log.d("onError", e.toString());
+            }
+
+            @Override
+            public void onNext(Boolean bool) {
+                Log.d("onNext", " in");
+            }
+        };
     }
 
-    private class PrefetchData extends AsyncTask<Void, Void, Void> {
 
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            // before making http calls
+    private void doLongOperation() {
 
-        }
-
-        @Override
-        protected Void doInBackground(Void... arg0) {
-
+        try {
             mAuth = FirebaseAuth.getInstance();
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
 
-            return null;
         }
-
-        @Override
-        protected void onPostExecute(Void result) {
-            super.onPostExecute(result);
-            Log.d("SplashScreen", " Start MainActivity");
-            Intent i = new Intent(SplashScreen.this,MainActivity.class);
-            startActivity(i);
-            finish();
-        }
-
     }
+
+
 
 }
