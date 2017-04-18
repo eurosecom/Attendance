155e9ac53b6407ea9e43f79872dfea6ed1f90052
eurosecom <eurosecom1@gmail.com>
2017-04-18 10:20:18 +0200

    Mysql saved Absences
    #15
    - onclicklistener to AbsServerAsAdapter.java Recyclerview

--- app/src/main/java/com/eusecom/attendance/AbsServerAsAdapter.java
+++ app/src/main/java/com/eusecom/attendance/AbsServerAsAdapter.java
@@ -51,7 +51,22 @@ public class AbsServerAsAdapter extends RecyclerView.Adapter<AbsServerAsAdapter.
   public void onBindViewHolder(AbsServerAsViewHolder holder, int position) {
       //holder.title.setText(mCheeses.get(position));
       holder.title.setText(mListabsserver.get(position).dmna);
-  }
+
+      holder.setClickListener(new AbsServerAsAdapter.AbsServerAsViewHolder.ClickListener() {
+          public void onClick(View v, int pos, boolean isLongClick) {
+              if (isLongClick) {
+
+                  // View v at position pos is long-clicked.
+                  Log.d("onLongClickListener", mListabsserver.get(pos).dmna);
+
+              } else {
+
+                  Log.d("onShortClickListener", mListabsserver.get(pos).dmna);
+              }
+          }
+      });
+
+  }//end onbindviewholder
 
     @Override
     public int getItemCount() {
@@ -66,12 +81,51 @@ public class AbsServerAsAdapter extends RecyclerView.Adapter<AbsServerAsAdapter.
     }
 
 
-  public static class AbsServerAsViewHolder extends RecyclerView.ViewHolder {
+  public static class AbsServerAsViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener{
     public final TextView title;
+      private ClickListener clickListener;
 
     public AbsServerAsViewHolder(View itemView) {
       super(itemView);
       title = (TextView) itemView.findViewById(R.id.datefrom);
+        itemView.setOnClickListener(this);
+        itemView.setOnLongClickListener(this);
     }
-  }
-}
+      /* Interface for handling clicks - both normal and long ones. */
+      public interface ClickListener {
+
+          /**
+           * Called when the view is clicked.
+           *
+           * @param v view that is clicked
+           * @param position of the clicked item
+           * @param isLongClick true if long click, false otherwise
+           */
+          public void onClick(View v, int position, boolean isLongClick);
+
+      }
+
+      /* Setter for listener. */
+      public void setClickListener(ClickListener clickListener) {
+          this.clickListener = clickListener;
+      }
+
+      @Override
+      public void onClick(View v) {
+
+          // If not long clicked, pass last variable as false.
+          clickListener.onClick(v, getPosition(), false);
+      }
+
+      @Override
+      public boolean onLongClick(View v) {
+
+          // If long clicked, passed last variable as true.
+          clickListener.onClick(v, getPosition(), true);
+          return true;
+      }
+
+
+  }//end class viewholder
+
+}//end class adapter
