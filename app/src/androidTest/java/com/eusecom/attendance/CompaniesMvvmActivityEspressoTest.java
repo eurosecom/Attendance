4c020ad979ef19361c5191d397d1a7fd3e110ab2
eurosecom <eurosecom1@gmail.com>
2017-08-15 16:47:57 +0200

    My Companies Activity
    #20
    - android instrument testing click on reczclerview and display control
    dialog

--- app/src/androidTest/java/com/eusecom/attendance/CompaniesMvvmActivityEspressoTest.java
+++ app/src/androidTest/java/com/eusecom/attendance/CompaniesMvvmActivityEspressoTest.java
@@ -6,50 +6,46 @@ import android.support.test.espresso.Root;
 import android.support.test.espresso.action.ViewActions;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
+import android.support.test.runner.AndroidJUnit4;
 import android.view.WindowManager;
-
 import org.hamcrest.Description;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.internal.matchers.TypeSafeMatcher;
+import org.junit.runner.RunWith;
 
-import static android.support.test.espresso.Espresso.closeSoftKeyboard;
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.longClick;
-import static android.support.test.espresso.action.ViewActions.pressBack;
-import static android.support.test.espresso.action.ViewActions.typeText;
-import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.isRoot;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static android.support.v7.widget.StaggeredGridLayoutManager.TAG;
-import static org.hamcrest.Matchers.allOf;
+
 
 //        On your device, under Settings->Developer options disable the following 3 settings:
 //        Window animation scale
 //        Transition animation scale
 //        Animator duration scale
 
+@RunWith(AndroidJUnit4.class)
 public class CompaniesMvvmActivityEspressoTest {
 
-    private String mStringToBetyped, mStringToBeChanged;
 
     @Rule
     public ActivityTestRule<CompaniesMvvmActivity> mActivityRule = new ActivityTestRule<>(CompaniesMvvmActivity.class);
 
     @Before
-    public void init() {
+    public void setUp() throws Exception {
 
         //get fragment
         mActivityRule.getActivity()
                 .getSupportFragmentManager().beginTransaction();
+
+
     }
 
     @Test
@@ -74,14 +70,25 @@ public class CompaniesMvvmActivityEspressoTest {
     @Test
     public void testRecyclerView() {
 
+
+
         //click on item recyclerview
         onView(withId(R.id.companies_list)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
         onView(withId(R.id.companies_list)).perform(RecyclerViewActions.actionOnItemAtPosition(1, click()));
         onView(withId(R.id.companies_list)).perform(RecyclerViewActions.actionOnItemAtPosition(0, longClick()));
 
+        //to verify if dialog is dispalyed To verify if dialog appears you can simply check if View with a text that present inside the dialog is shown:
+        onView(withText("company Id 12345678")).check(matches(isDisplayed()));
+
         onView(isRoot()).perform(ViewActions.pressBack());
 
         onView(withId(R.id.companies_list)).perform(RecyclerViewActions.actionOnItemAtPosition(1, longClick()));
+
+        //to verify if dialog is dispalyed To verify if dialog appears you can simply check if View with a text that present inside the dialog is shown:
+        onView(withText("company Id 44551142")).check(matches(isDisplayed()));
+
+        //To click on dialogs button do this (button1 - OK, button2 - Cancel):
+        onView(withId(android.R.id.button1)).perform(click());
 
 
     }
@@ -108,6 +115,7 @@ public class CompaniesMvvmActivityEspressoTest {
         }
 
     }
+
 
 
 }
